c922e16c6e0e0b5312c5b6220988a9ab6f454886
Ryusuke Hiraizumi <ryu@ryu-mba.local>
2024-01-10 12:18:18 +0900

    ページング機能の実装（一覧表示とカテゴリ検索は混在してるから要修正）

--- src/main/java/com/example/productmanagementex/controller/ItemController.java
+++ src/main/java/com/example/productmanagementex/controller/ItemController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.example.productmanagementex.service.CategoryService;
 import com.example.productmanagementex.service.ItemService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequestMapping({ "", "/" })
@@ -25,20 +24,26 @@ public class ItemController {
         return "redirect:/itemList";
     }
 
-    @GetMapping("itemList")
-    public String toItemList(@RequestParam(defaultValue = "1")int page, Model model) {
+    @RequestMapping("itemList")
+    public String toItemList(@RequestParam(defaultValue = "1") int page, Model model) {
         model.addAttribute("itemList", itemService.findAllItems(page));
-        model.addAttribute("itemListSize", itemService.itemListSize());
+        int itemListSize = itemService.itemListSize();
+        int totalPage = itemListSize / 30 + 1;
+        model.addAttribute("totalPage", totalPage);
+        model.addAttribute("currentPage", page);
         model.addAttribute("categoryList", categoryService.findAllUniqueCategory());
         return "list";
     }
 
-    @PostMapping("searchItemList")
+    @RequestMapping("searchItemList")
     public String toSearchItemList(String name, String brand, String parentCategory, String childCategory,
-            String grandCategory, Model model, @RequestParam(defaultValue = "1")int page) {
+            String grandCategory, Model model, @RequestParam(defaultValue = "1") int page) {
         model.addAttribute("itemList",
                 itemService.searchItems(name, brand, parentCategory, childCategory, grandCategory, page));
-        model.addAttribute("itemListSize", itemService.searchItemsSize(name, brand, parentCategory, childCategory, grandCategory));
+        int itemListSize = itemService.searchItemsSize(name, brand, parentCategory, childCategory, grandCategory);
+        int totalPage = itemListSize / 30 + 1;
+        model.addAttribute("totalPage", totalPage);
+        model.addAttribute("currentPage", page);
         model.addAttribute("categoryList", categoryService.findAllUniqueCategory());
 
         return "list";
