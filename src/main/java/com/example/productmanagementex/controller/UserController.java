dc2b6e0b4a1290156009c27b3ee0c1b90fa06242
Ryusuke Hiraizumi <ryu@ryu-mba.local>
2024-01-11 12:59:54 +0900

    controllerクラスの作成まで

---
+++ src/main/java/com/example/productmanagementex/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.example.productmanagementex.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.productmanagementex.form.UserForm;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping("")
+    public String index(UserForm form, Model model) {
+        model.addAttribute("userForm", form);
+        return "register";
+    }
+
+    @PostMapping("register")
+    public String registerUser(@Validated UserForm form, BindingResult rs, Model model) {
+        if (rs.hasErrors()) {
+            return index(form, model);
+        }
+
+        service.registerUser(form);
+
+        return "confirm";
+    }
+}
