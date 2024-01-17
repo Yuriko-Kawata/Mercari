6f99a37421f1b32c31d356fde39c28422666f141
Ryusuke Hiraizumi <ryu@ryu-mba.local>
2024-01-17 10:32:11 +0900

    パスワードのハッシュ化

--- src/main/java/com/example/productmanagementex/service/UserService.java
+++ src/main/java/com/example/productmanagementex/service/UserService.java
@@ -1,5 +1,10 @@
 package com.example.productmanagementex.service;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +22,20 @@ public class UserService {
     private UserRepository repository;
 
     public void registerUser(UserForm form) {
+        try {
+            // SHA-256メッセージダイジェストのインスタンスを取得
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+            // パスワードをバイト配列に変換してハッシュ化
+            byte[] hashedBytes = md.digest(form.getPassword().getBytes(StandardCharsets.UTF_8));
+
+            // Base64エンコーディングを使用して、バイト配列を文字列に変換
+            form.setPassword(Base64.getEncoder().encodeToString(hashedBytes));
+        } catch (NoSuchAlgorithmException e) {
+            // SHA-256アルゴリズムが見つからない場合のエラー処理
+            e.printStackTrace();
+        }
+
         User user = new User();
         BeanUtils.copyProperties(form, user);
 
